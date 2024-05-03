458210ca2cfb15a6abfadbe483b099cc34c93ba8
sun-sunDayz <kill.yanggochi@gmail.com>
2024-05-03 18:06:37 +0900

    feature 특정기사조회

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('article',)
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    
+    comments = CommentSerializer(many=True, read_only=True)
+    comments_count = serializers.IntegerField(source='comments.count', read_only=True)
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+    
+    
--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ArticleAPIView.as_view()),
+    path("<int:article_pk>/", views.ArticleDetailAPIView.as_view())
 ]
