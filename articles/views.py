70fc921c278067e99dd6d58a3f251d8d2ff275b8
김병민 <gimbyeongmin@byeong.local>
2024-05-03 21:10:21 +0900

    좋아요 기능 구현 검증 필요

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
-
+from rest_framework.response import Response
+from .models import Article
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class ArticleAPIView(APIView):
     pass
+
+
+
+class LikeyArticleAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, article_pk):
+        article = get_object_or_404(Article,pk=article_pk)
+        user = request.user.id
+        article.likey.add(user)
+        return Response({'message': '좋아요'},status=200)
+        
+
+    def delete(self, request, article_pk):
+        article = get_object_or_404(Article,pk=article_pk)
+        user = request.user.id
+        article.likey.remove(user)
+        return Response({'message': '좋아요 취소'},status=200)
