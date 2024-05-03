5c472f29e4af69a781a433523706bdf823198f05
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-05-03 16:54:28 +0900

    feature profile method:GET response: (username, date_joined, point, introduce, email)

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 
 class AccountAPIView(APIView):
     def post(self, request):
         """
-        회원가입
+        회원가입(권한 : 모든 유저)
         """
         data = request.data
         username = data.get('username')
@@ -19,7 +20,23 @@ class AccountAPIView(APIView):
         # 이미 가입한 유저명 제한
         if get_user_model().objects.filter(username=username).exists():
             return Response({"error": "이미 가입한 계정입니다."}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         # 유저 등록
-        get_user_model().objects.create_user(username=username, password=data.get("password"))
+        get_user_model().objects.create_user(
+            username=username, password=data.get("password"))
         return Response({"username": username}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def profile(request, username):
+    """
+    프로필 조회(권한 : 모든 유저)
+    """
+    user = get_user_model().objects.filter(username=username).first()
+    return Response({
+        "username": user.username,
+        "date_joined": user.date_joined,
+        "point": user.point,
+        "introduce": user.introduce,
+        "email": user.email
+    })
