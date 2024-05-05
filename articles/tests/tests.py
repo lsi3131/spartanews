460066e9be072175c630257ad89d7b7f10881fcb
sang-il <lsi9393@naver.com>
2024-05-05 16:13:43 +0900

    feat comment 대댓글 * 중간상태 1차 저장

--- articles/tests/tests.py
+++ articles/tests/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from ..models import *
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+import json
 
 User = get_user_model()
 
@@ -21,19 +23,22 @@ class CommentAPIViewTestCase(TestCase):
         comment_child2 = Comment.objects.create(article=article, content='comment child2', parent_comment=comment_root)
 
         Comment.objects.create(article=article, content='comment grand child1',
-                                                       parent_comment=comment_child1)
+                               parent_comment=comment_child1)
         Comment.objects.create(article=article, content='comment grand child2',
-                                                       parent_comment=comment_child1)
+                               parent_comment=comment_child1)
 
         children = comment_root.get_children()
         self.assertEqual(2, len(children))
         self.assertEqual(2, len(comment_child1.get_children()))
         self.assertEqual(0, len(comment_child2.get_children()))
 
-    def test_create_child_comments_and_get_children_with_view(self):
+    def test_get_view(self):
         self.login_as_user()
         article = Article.objects.create(title='title', article_type='news', article_link=self.article_link,
                                          content='content', author=self.user)
 
+        comment = Comment.objects.create(article=article, content='comment')
+        url = reverse('articles:comment_detail', args=[article.id, comment.id])
 
-
+        response = self.client.get(url, content_type='application/json')
+        self.assertEqual('comment', response.data['content'])
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from django.http.request import HttpRequest
 from rest_framework.response import Response
 from rest_framework import status
+from .models import *
+from .serializers import *
 
 
 # Create your views here.
@@ -12,7 +14,13 @@ class ArticleAPIView(APIView):
 
 class CommentDetailAPIView(APIView):
     def get(self, request: HttpRequest, article_pk, comment_pk):
-        return Response({'message': 'message'}, status=status.HTTP_200_OK)
+        article = get_object_or_404(Article, id=article_pk)
+        comment = get_object_or_404(Comment, id=comment_pk)
+
+        serializer = CommentSerializer(comment)
+        # serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def put(self, request: HttpRequest, article_pk, comment_pk):
         return Response({'message': 'message'}, status=status.HTTP_200_OK)
