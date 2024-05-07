0ff05c019bf212d57e861d005557c53ae740ebe1
sun-sunDayz <kill.yanggochi@gmail.com>
2024-05-07 14:18:10 +0900

    welcome to hell

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
