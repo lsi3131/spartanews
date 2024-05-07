da8625fd66ce0e9339bc14120fbf54b191c9cd94
sang-il <lsi9393@naver.com>
2024-05-07 14:42:41 +0900

    feat signupform * 부트스트랩 패키지, import 추가 * 회원가입 페이지 작성 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import LoginForm from './Components/LoginForm/LoginForm'
